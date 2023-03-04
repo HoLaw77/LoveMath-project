b7e2b2763a10de28a015920f97d9f9dd1920cbcb
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-04 10:19:35 +0000

    set up game structure

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+function displayAddidtionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
