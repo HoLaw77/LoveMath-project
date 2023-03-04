109e9b0d255488360c64940ba148f720a7060080
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-04 13:24:11 +0000

    Displaying addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,24 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     // random number between 1 to 25//
 let num1 = Math.floor(Math.random() * 25) +1;
 let num2 = Math.floor(Math.random() * 25) +1;
+
+if(gameType === "addition"){
+    displayAddidtionQuestion(num1, num2);
+} else {alert(`unknown game type: $(gameType)`); throw `unknown game type: $(gameType).Aborting`;}
 }
 
 
@@ -38,7 +43,10 @@ function incrementScore(){
 function incrementWrongAnswer(){
 
 }
-function displayAddidtionQuestion(){
+function displayAddidtionQuestion(operand1, operand2){
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 
 }
 function displaySubtractQuestion(){
